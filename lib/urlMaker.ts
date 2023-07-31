40a984093318faa77bbca32d2772ce5e060b0b27
Temporal 76 <marc.pollet76@gmail.com>
2023-07-31 09:57:33 +0200

    preparing for vercel deployment

--- lib/urlMaker.ts
+++ lib/urlMaker.ts
@@ -1,4 +1,4 @@
 export default function urlMaker() {
-    if (process.env.NODE_ENV === "production") return "ADD SERVER URL HERE"
+    if (process.env.NODE_ENV === "production") return "https://back-end-qr-code.fly.dev/"
     return "http://localhost:3000/"
 }
